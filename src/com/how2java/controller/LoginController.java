1ac4cb9051c09651234e3e26f1ef82eb9a04e15a
TzWind <1132808592@qq.com>
2019-09-23 17:22:17 +0800

    0

--- src/com/how2java/controller/LoginController.java
+++ src/com/how2java/controller/LoginController.java
@@ -1,21 +1,13 @@
 package com.how2java.controller;
 
 
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.how2java.pojo.Category;
 import com.how2java.pojo.User;
 import com.how2java.service.CategoryService;
 
